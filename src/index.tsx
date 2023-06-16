e5b73b74d916cae9b0026f97035eb4c54dbda62c
Tim Cai <45728518+timeach@users.noreply.github.com>
2023-06-16 08:57:13 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import rootStore from "./redux/store";
 import axios from "axios";
 import { PersistGate } from "redux-persist/integration/react"
 
-axios.defaults.headers['x-icode'] = 'J17F9B6E878C54BCD';
+axios.defaults.headers['x-icode'] = 'J4DBCE68D3DEE9D35';
 
 ReactDOM.render(
   <React.StrictMode>
